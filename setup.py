7fe0c638a1a31f16e3e73d48041aaa069dd79e93
Janis Dalins <janis.dalins@afp.gov.au>
2019-09-05 13:40:51 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyMIH',
+    version='0.5.0',
+    packages=[''],
+    url='https://github.com/AiLECS/pyMIH',
+    license='MIT',
+    author='Janis Dalins',
+    author_email='janis.dalins@monash.edu',
+    description='Python implementation of Multiple-Index Hashing, an efficient technique for accelerating fuzzy hash lookups. '
+)
